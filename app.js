5dca237e393463ce947861f46f9b8410b1037243
Peter Shank <peter@shank.com>
2018-02-08 22:08:43 +0000

    testing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-var port = process.env.PORT || 8080; // if deployed to Heroku, process.env.PORT will be set dynamically
+// if deployed to Heroku, process.env.PORT will be set dynamically to some value you can't predict,
+// even though your publicly-visible app will be served on port 80
+var port = process.env.PORT || 8080; 
 
-app.get('/', (req, res) => res.send('Howdy from port ' + port));
+app.get('/', (req, res) => res.send('Greetings from excellent and outstanding port ' + port));
 
 app.listen(port, () => console.log('Listening on port ' + port));
