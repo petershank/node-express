d098e34c4cf1da22b381e466b2f244180012e053
Peter Shank <peter@shank.com>
2018-02-12 21:29:43 +0000

    improving comments

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// a bare bones http server implemented in node and express
+
 const express = require('express');
 const app = express();
 
-// if deployed to Heroku, process.env.PORT will be set dynamically to some value you can't predict,
-// even though your publicly-visible app will be served on port 80
+// in some deployment environments, such as Heroku, process.env.PORT will be set dynamically 
+// to a value you can't control,
+// even though your publicly visible app will be served on port 80
 var port = process.env.PORT || 8080; 
 
-app.get('/', (req, res) => res.send('Greetings from excellent and outstanding port ' + port));
+app.get('/', (req, res) => res.send('Greetings from outstanding port ' + port));
 
 app.listen(port, () => console.log('Listening on port ' + port));
