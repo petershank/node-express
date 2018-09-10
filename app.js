2b302a7bbaa68a066cc4afd05fb3af554edb4957
Peter Shank <peter@shank.com>
2018-09-10 21:02:44 +0000

    yet another trivial response content change

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const app = express();
 
 var port = process.env.PORT || 8080; 
 
-app.get('/', (req, res) => res.send('Hello, world.));
+app.get('/', (req, res) => res.send('Hello, world.'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
