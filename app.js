30de09242ee39c816f0c35d7937c20e31e6f6fee
Peter Shank <peter@mac-mini.local>
2019-01-04 12:50:56 -0800

    change hardcoded response

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const app = express();
 
-// in some deployment environments, such as Heroku, process.env.PORT will be set dynamically 
+// in some deployment environments, such as Heroku, process.env.PORT will be set dynamically
 // to a value you can't control, even though your publicly visible app will be served on port 80 or so
 
-var port = process.env.PORT || 8080; 
+var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello, world.'));
+app.get('/', (req, res) => res.send('Hello, awesome world.'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
