29748637ff65f55d365dc2254a256ec0a20fc98d
Peter Shank <peter@shank.com>
2019-01-07 15:06:05 +0000

    change response content

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello, world from a server file.'));
+app.get('/', (req, res) => res.send('Hello, world.'));
 
 app.listen(port, () => console.log('Listening on port ' + port));
